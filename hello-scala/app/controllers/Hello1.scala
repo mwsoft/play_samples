b5175921b6badda8dff709f10b170e262012872e
masato <masato@RISU.(none)>
2013-07-06 18:48:35 +0900

    fix indent

--- hello-scala/app/controllers/Hello1.scala
+++ hello-scala/app/controllers/Hello1.scala
@@ -4,28 +4,27 @@ import play.api.mvc.Controller
 import play.api.mvc.Action
 
 object Hello1 extends Controller {
-  
+
   /**
    *  text/plain で hello world を表示する。
    *  たぶんこれが一番簡単な hello world の出し方だと思います。
    */
   def index = Action {
-    Ok( "hello world" ) 
-    }
-  
+    Ok("hello world")
+  }
+
   /**
    * views.hello2 を呼び出して hello world を表示する。
    */
   def hello = Action {
-    Ok( views.html.hello2.render ) 
-    }
+    Ok(views.html.hello2.render)
+  }
 
   /**
    * views.hello3 に引数として hello world を渡して表示する。
    */
   def hello2 = Action {
-    Ok( views.html.hello3.render( "hello world" ) ) 
-    }
-  
-    
+    Ok(views.html.hello3.render("hello world"))
+  }
+
 }
--- hello-scala/app/controllers/Hello2.scala
+++ hello-scala/app/controllers/Hello2.scala
@@ -9,15 +9,15 @@ object Hello2 extends Controller {
   /**
    * routes から引数として hello world を受け取って表示する。
    */
-  def hello( msg: String ) = Action {
-    Ok( views.html.hello3.render( msg ) ) 
+  def hello(msg: String) = Action {
+    Ok(views.html.hello3.render(msg))
   }
-  
+
   /**
    * routes から引数として hello world を受け取って表示する。
    * encode されたスペース（%20）が decode されなかったので明示的に decode。
    */
-  def helloWithDecode( msg: String ) = Action {
-     Ok( views.html.hello3.render( URLDecoder.decode( msg, "utf-8" ) ) )
+  def helloWithDecode(msg: String) = Action {
+    Ok(views.html.hello3.render(URLDecoder.decode(msg, "utf-8")))
   }
 }
